a7b86289e3bf31044d11810d832585093d9fd125
osama-cybage <osama.mohd1214@gmail.com>
2024-09-29 18:38:34 +0530

    Update CurrencyExchangeApplication.java

--- src/main/java/com/currency/CurrencyExchangeApplication.java
+++ src/main/java/com/currency/CurrencyExchangeApplication.java
@@ -2,10 +2,9 @@ package com.currency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @EnableCaching
 public class CurrencyExchangeApplication {
 
