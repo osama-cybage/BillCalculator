e3b122405c4062674707eaba855e08f03511e9d7
osama-cybage <osama.mohd1214@gmail.com>
2024-10-09 18:23:52 +0530

    Update CurrencyServiceImpl.java

--- src/main/java/com/currency/service/CurrencyServiceImpl.java
+++ src/main/java/com/currency/service/CurrencyServiceImpl.java
@@ -1,18 +1,13 @@
 package com.currency.service;
 
-import java.util.Objects;
 import java.util.Optional;
 
 import com.currency.util.CurrencyParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.currency.model.Bill;
 import com.currency.util.BillCalculator;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
